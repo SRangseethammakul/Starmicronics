d91e0451cc191fb4a15b52b6494fae3487651997
Suttipong Rangseethammakul <51299265+SRangseethammakul@users.noreply.github.com>
2020-12-18 12:47:01 +0700

    Update warranty_SystemImport.php
    intval

--- app/Imports/warranty_SystemImport.php
+++ app/Imports/warranty_SystemImport.php
@@ -18,7 +18,7 @@ class warranty_SystemImport implements ToModel //,WithValidation
     public function model(array $row)
     {
         try{
-            dd($row[8]->format('Y-m-d'));
+            dd(intval($row[8]->format('Y-m-d')));
               return new warranty_system([
                     'serial_number'     => $row[0],
                     'good_group'    => $row[1],
