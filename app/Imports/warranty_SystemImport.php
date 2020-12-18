31954b7a1bc6eec7faaf5411a563cb1674cf93e5
Suttipong Rangseethammakul <51299265+SRangseethammakul@users.noreply.github.com>
2020-12-18 11:29:57 +0700

    Update warranty_SystemImport.php
    add try catch

--- app/Imports/warranty_SystemImport.php
+++ app/Imports/warranty_SystemImport.php
@@ -14,19 +14,24 @@ class warranty_SystemImport implements ToModel
     */
     public function model(array $row)
     {
-        return new warranty_system([
-            'serial_number'     => $row[0],
-            'good_group'    => $row[1],
-            'good_code'    => $row[2],
-            'good_description'    => $row[3],
-            'cartoon'    => $row[4],
-            'shipped_qty'    => $row[5],
-            'invoice'    => $row[6],
-            'customer'    => $row[7],
-            'shipped_date'    => \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row[8])->format('Y-m-d'),
-            'location'    => $row[9],
-            'expired_date'    => \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row[10])->format('Y-m-d'),
-            'Warranty'    =>  $row[11]
-        ]);
+        try{
+              return new warranty_system([
+                    'serial_number'     => $row[0],
+                    'good_group'    => $row[1],
+                    'good_code'    => $row[2],
+                    'good_description'    => $row[3],
+                    'cartoon'    => $row[4],
+                    'shipped_qty'    => $row[5],
+                    'invoice'    => $row[6],
+                    'customer'    => $row[7],
+                    'shipped_date'    => \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row[8])->format('Y-m-d'),
+                    'location'    => $row[9],
+                    'expired_date'    => \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row[10])->format('Y-m-d'),
+                    'Warranty'    =>  $row[11]
+            ]);
+        }catch (\Maatwebsite\Excel\Validators\ValidationException $failures)
+        {
+            return view ('welcome', compact('failures'));
+        }
     }
 }
